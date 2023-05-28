ab206cad334305f2b8251b37f08ef47e9cddc674
Ayush Bachna <21051614@kiit.ac.in>
2023-05-28 12:52:41 +0530

    second commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login page</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="User/Utility/style.css">
 </head>
 
@@ -59,7 +59,7 @@
 
         </div>
     </div>
-    <script src="js/bootstrap.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
 </body>
 
 </html>
